e21b5e47add2c84b64c09837ec92842c69777ebe
Twometer <twometer@outlook.de>
2020-08-01 23:15:37 +0200

    More design

--- src/tk/myanimes/UserServlet.java
+++ src/tk/myanimes/UserServlet.java
@@ -12,6 +12,6 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/user.jsp").forward(req, resp);
+        req.getRequestDispatcher("/user.html").forward(req, resp);
     }
 }
