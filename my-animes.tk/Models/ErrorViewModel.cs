4911d6892b9e49f0ef288c68772b9db1568422ae
Twometer <twometer@outlook.de>
2020-08-21 16:31:29 +0200

    Remove auto-generated auth code

---
+++ my-animes.tk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_animes.tk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
