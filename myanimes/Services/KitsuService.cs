840f15b2ec07e47d26c154414a0d9f7d5a9bf4c7
Twometer <twometer@outlook.de>
2020-09-06 23:07:06 +0200

    Create http client for the kitsu service

--- myanimes/Services/KitsuService.cs
+++ myanimes/Services/KitsuService.cs
@@ -1,27 +1,50 @@
 ﻿using myanimes.Database.Entities.Animes;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace myanimes.Services
 {
     public class KitsuService
     {
+        private const string UserAgent = "myanimes/ApiClient/2.0";
+        private readonly IWebProxy NullProxy = new WebProxy();
+
         public async Task<IEnumerable<Anime>> GetTrendingAnimes()
         {
-            // https://kitsu.io/api/edge/trending/anime
+            var json = await HttpGet("https://kitsu.io/api/edge/trending/anime");
+
             return null; // TODO
         }
 
         public async Task<Anime> GetAnime(string slug)
         {
-            // https://kitsu.io/api/edge/anime?filter[slug]=$SLUG$&include=genres,productions.company,animeProductions.producer,episodes,streamingLinks,characters.character
+            var encodedSlug = WebUtility.UrlEncode(slug);
+            var json = await HttpGet($"https://kitsu.io/api/edge/anime?filter[slug]={encodedSlug}&include=genres,productions.company,animeProductions.producer,episodes,streamingLinks,characters.character");
+
             return null; // TODO
         }
 
         public async Task<IEnumerable<SearchResult>> Search(string query)
         {
-            // https://kitsu.io/api/edge/anime?filter[text]=$QUERY$&fields[anime]=slug,titles,posterImage
+            var encodedQuery = WebUtility.UrlEncode(query);
+            var json = await HttpGet($"https://kitsu.io/api/edge/anime?filter[text]={encodedQuery}&fields[anime]=slug,titles,posterImage");
+            
             return null; // TODO
+        }
+
+        private async Task<string> HttpGet(string url)
+        {
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            request.UserAgent = UserAgent;
+            request.Proxy = NullProxy;
+
+            using var response = await request.GetResponseAsync();
+            using var stream = response.GetResponseStream();
+            using var reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
         }
     }
 }
