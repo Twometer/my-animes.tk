fddc00c27a37ac33886bded2de50975b3db58108
Twometer <twometer@outlook.de>
2020-09-07 22:34:56 +0200

    Start work on the kitsu parser

--- myanimes/Services/KitsuService.cs
+++ myanimes/Services/KitsuService.cs
@@ -1,7 +1,11 @@
-﻿using myanimes.Models;
+﻿using Microsoft.AspNetCore.Http.Features;
+using myanimes.Database.Entities.Animes;
+using myanimes.Models;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -24,14 +28,46 @@ namespace myanimes.Services
             var encodedSlug = WebUtility.UrlEncode(slug);
             var json = await HttpGet($"https://kitsu.io/api/edge/anime?filter[slug]={encodedSlug}&include=genres,productions.company,animeProductions.producer,episodes,streamingLinks,characters.character");
 
-            return null; // TODO
+            var includedObjects = json["included"] as JArray;
+            var attributes = json["data"][0]["attributes"];
+            var titles = attributes["titles"] as JObject;
+
+            var animeTitles = new List<Title>();
+            foreach (var item in titles)
+                animeTitles.Add(new Title { Language = item.Key, Text = item.Value.Value<string>() });
+
+            var animeGenres = includedObjects.Where(o => o.Value<string>("type") == "genre")
+                .Select(g => new Genre { Name = g.Value<string>("name"), Slug = g.Value<string>("slug") });
+
+            var anime = new Anime
+            {
+                Slug = attributes.Value<string>("slug"),
+                CanonicalTitle = attributes.Value<string>("canonicalTitle"),
+                Titles = animeTitles,
+                StartDate = attributes.Value<DateTime>("startDate"),
+                EndDate = attributes.Value<DateTime>("endDate"),
+                Synopsis = attributes.Value<string>("synopsis"),
+                CoverImageUrl = attributes["coverImage"].Value<string>("small"),
+                ThumbnailUrl = attributes["coverImage"].Value<string>("tiny"),
+                TrailerYoutubeId = attributes.Value<string>("youtubeVideoId"),
+                AgeRating = attributes.Value<string>("ageRatingGuide"),
+                Nsfw = attributes.Value<bool>("nsfw"),
+                EpisodeCount = attributes.Value<int>("episodeCount"),
+                EpisodeLength = attributes.Value<int>("episodeLength"),
+                TotalLength = attributes.Value<int>("totalLength"),
+                Status = attributes.Value<ShowStatus>("status"),
+                Type = attributes.Value<ShowType>("subtype")
+            };
+
+
+            return anime; // TODO
         }
 
         public async Task<IEnumerable<SearchResult>> Search(string query)
         {
             var encodedQuery = WebUtility.UrlEncode(query);
             var json = await HttpGet($"https://kitsu.io/api/edge/anime?filter[text]={encodedQuery}&fields[anime]=slug,titles,posterImage");
-            
+
             return null; // TODO
         }
 
