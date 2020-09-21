ee5132f0a38ab1b992a0194bf071e32480cac102
Twometer <twometer@outlook.de>
2020-09-21 15:32:40 +0200

    Create placeholder classes for all pages

---
+++ myanimes/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myanimes.Controllers
+{
+    [Route("~/login/{action=Index}")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ myanimes/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myanimes.Controllers
+{
+    [Route("~/register/{action=Index}")]
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
