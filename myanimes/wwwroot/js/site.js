421157037e8e1261747af40fd4f0fee7e932f3a2
Twometer <twometer@outlook.de>
2020-09-25 14:23:15 +0200

    User-friendly AJAX forms

--- myanimes/wwwroot/js/site.js
+++ myanimes/wwwroot/js/site.js
@@ -1,5 +1,11 @@
 ﻿(function () {
 
+    var spinner = buildSpinner();
+
+    function buildSpinner() {
+        return $('<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>');
+    }
+
     function serializeForm($form) {
         var array = $form.serializeArray();
         var obj = {};
@@ -16,16 +22,23 @@
         $form.on('submit', function (e) {
             e.preventDefault();
 
+            var button = $form.find("button[type=submit]");
+            var oldContent = button.html();
+            button.prop('disabled', true);
+            button.html(spinner);
+
             var url = window.location.href;
             var payload = serializeForm($form);
 
             function ajaxCallback(data, status) {
-                handleResponse($form, data, status === "success");
+                handleResponse($form, data, status);
+                button.prop('disabled', false);
+                button.html(oldContent);
             }
 
             jQuery.ajax({
                 type: "POST",
-                urL: url,
+                url: url,
                 data: payload,
                 dataType: "json",
                 contentType: "application/json",
@@ -35,11 +48,17 @@
         });
     }
 
-    function handleResponse($form, data, success) {
+    function handleResponse($form, data, status) {
         var response = data.responseJSON;
+        var errorBox = $form.find(".error-message");
 
-        if (!success) {
-            console.error(response.message);
+        if (!response) {
+            errorBox.html("Internal server error");
+            console.error("Cannot read response:", status);
+        } else if (status !== "success") {
+            var message = response.message;
+            errorBox.html(message);
+            console.error(message);
         } else {
             console.log("Okay!");
         }
