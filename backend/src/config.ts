bfb8b3716fe259caf37af7a28605be2c64ca6f58
Twometer <twometer@outlook.de>
2021-06-21 15:47:24 +0200

    Backend: Switch to env-based configuration

---
+++ backend/src/config.ts
@@ -0,0 +1,9 @@
+import {Config} from "@twometer/webcore";
+
+export default Config.from({
+    HTTP_PORT: 8090,
+    CORS_ORIGIN: "http://localhost:8080",
+    SESSION_SECURE: false,
+    SESSION_SECRET: "secret",
+    DB_URL: "mongodb://localhost:27017/myanimes"
+}).read()
