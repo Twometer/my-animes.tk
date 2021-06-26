c521ac1527098c0cbd565558b53faea8aacb7b0a
Twometer <twometer@outlook.de>
2021-06-26 17:30:59 +0200

    Backend: Use query parameter for search

--- backend/src/endpoints/search.ts
+++ backend/src/endpoints/search.ts
@@ -1,11 +1,15 @@
-import {Webapp} from "@twometer/webcore";
+import {Webapp, error} from "@twometer/webcore";
 import * as kitsu from "../kitsu";
 
 export default function (webapp: Webapp) {
     const route = webapp.route('/api/search')
 
-    route.get('/:query', async (req, resp) => {
-        let results = await kitsu.search(req.params.query);
+    route.get('/', async (req, resp) => {
+        let query = <string>req.query.q;
+        if (!query || query.trim() == '')
+            return error(resp, 400, 'Missing parameter q')
+
+        let results = await kitsu.search(query);
         resp.json(results);
     })
 }
