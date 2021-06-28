fe21f882993e2d471bf01387eff0ea9f1d30470a
Twometer <twometer@outlook.de>
2021-06-28 17:23:00 +0200

    Backend: User API - Auth conditions

--- backend/src/endpoints/user.ts
+++ backend/src/endpoints/user.ts
@@ -1,3 +1,4 @@
+import * as db from '../database'
 import {Webapp, error} from "@twometer/webcore";
 
 export default function (webapp: Webapp) {
@@ -5,11 +6,15 @@ export default function (webapp: Webapp) {
 
     // Create a new user
     route.post('/new', async (req, resp) => {
+        if (req.session.loggedIn)
+            return error(resp, 400, 'Already logged in')
 
     })
 
     // Delete a user account [Requires auth]
     route.delete('/:username', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     })
 
@@ -20,11 +25,15 @@ export default function (webapp: Webapp) {
 
     // Update the profile for the given username [Requires auth]
     route.put('/:username/profile', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     });
 
     // Change the password for the given username [Requires auth]
     route.put('/:username/password', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     });
 
@@ -35,16 +44,22 @@ export default function (webapp: Webapp) {
 
     // Add a new anime to the watchlist [Requires auth]
     route.post('/:username/list/new', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     });
 
     // Update the data for one item on the watchlist [Requires auth]
     route.put('/:username/list/:itemId', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     });
 
     // Remove an item from the watchlist [Requires auth]
     route.delete('/:username/list/:itemId', async (req, resp) => {
+        if (!req.session.loggedIn || req.session.username != req.params.username)
+            return error(resp, 401)
 
     });
 }
